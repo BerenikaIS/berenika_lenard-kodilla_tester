278b3fd7a08dc3ff7c84f0d614513498338f53ba
Berenika <berenika.lenard@gmail.com>
2022-10-24 16:04:26 +0000

    Main Extended DW

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MainExtended {
-}
+public class MainExtended {
+    public static void main(String[] args) {
+            String myCity = "Keflavik";
+            int cityPopulation = 15930;
+            boolean theStormIsComing = false;
+            boolean theWinterIsComing = true;
+            double monthsOfDarkness = 6.5;
+            char catNameFirstLetter = 'M';
+            System.out.println(myCity);
+            System.out.println(cityPopulation);
+            System.out.println(theStormIsComing);
+            System.out.println(theWinterIsComing);
+            System.out.println(monthsOfDarkness);
+            System.out.println(catNameFirstLetter);
+        }
+    }
