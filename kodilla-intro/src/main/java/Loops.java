13bc236abdf4164339b1173b36214b51f5c27bae
Berenika <berenika.lenard@gmail.com>
2022-11-11 15:44:12 +0000

    Grades

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,9 +1,7 @@
 public class Loops {
     public static void main(String[] args) {
-        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
-        int numberOfElements = names.length;
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names [i]);
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
         }
     }
 }
